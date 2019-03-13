cf451176daebb06fa7de73f1184ae06e25ce5131
My420 <My420V@gmail.com>
2019-03-13 18:32:32 +0300

    file destination test

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,19 @@
 "use strict";
 const gulp = require("gulp");
 
-gulp.task("hello", function(callback) {
-  console.log(`helloo`);
-  callback();
+gulp.task("start", function(callback) {
+  return gulp
+    .src("src/**/*.*")
+    .on("data", function(data) {
+      console.log(data);
+    })
+    .pipe(
+      gulp.dest(function(file) {
+        if (file.extname == ".js") {
+          return "dest/js";
+        } else if (file.extname == ".css") {
+          return "dest/css";
+        } else return "dest";
+      })
+    );
 });
-
-gulp.task("1", function(callback) {
-  console.log(`1`);
-  callback();
-});
-
-gulp.task("2", function(callback) {
-  console.log(`2`);
-  callback();
-});
-
-gulp.task("3", function(callback) {
-  console.log(`3`);
-  callback();
-});
-
-gulp.task("all", gulp.series("hello", "1", "2", "3"));
