7b5666ae8e7081354dad289a45330443c08a1492
My420 <My420V@gmail.com>
2019-03-18 15:51:11 +0300

    complete 5 and 6 screencast

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const concat = require("gulp-concat");
 const debug = require("gulp-debug");
 const sourcemaps = require("gulp-sourcemaps");
 const del = require("del");
+const newer = require("gulp-newer");
 
 gulp.task("style", function() {
   return gulp
@@ -20,7 +21,18 @@ gulp.task("clean", function() {
 });
 
 gulp.task("assets", function() {
-  return gulp.src("src/assets/**/*.*").pipe(gulp.dest("build"));
+  return gulp
+    .src("src/assets/**/*.*", { since: gulp.lastRun("assets") }) // копируем только то что изменилось с последнего запуска "assets"
+    .pipe(newer("build"))
+    .pipe(debug({ title: "assets" }))
+    .pipe(gulp.dest("build"));
 });
 
 gulp.task("build", gulp.series("clean", gulp.parallel("assets", "style")));
+
+gulp.task("watch", function() {
+  gulp.watch("src/scss/**/*.*", gulp.series("style"));
+  gulp.watch("src/assets/**/*.*", gulp.series("assets"));
+});
+
+gulp.task("dev", gulp.series("build", "watch"));
